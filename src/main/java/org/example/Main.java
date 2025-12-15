305168b3cd44d071a9addda057145b836054ade1
Stz93 <58509404+Stz93@users.noreply.github.com>
2025-12-15 09:25:47 +0600

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,138 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Количество потоков выделенное для многопоточных операций
+     */
+    final static int THREAD_NUMBER = 4;
+    /**
+     * Флаг для прерывания выполнения приложения
+     */
+    static boolean flag = true;
+
+    public static void main(String[] args) {
+        List<Student> studentList = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (flag) {
+            try {
+                processRequest(scanner);
+            } catch (final IllegalStateException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    /**
+     * Обработка запроса пользователя.
+     *
+     * @param scanner требуется передать {@link Scanner} читающий из консоли.
+     * @throws IllegalStateException выбрасываем исключения для некорректных вводов в консоль.
+     */
+    public static void processRequest(Scanner scanner) throws IllegalStateException {
+        System.out.println("MENU :\t1 to input");
+        System.out.println("\t\t2 to output");
+        System.out.println("\t\t3 to sort");
+        System.out.println("\t\t4 to count entries");
+        System.out.println("\t\t0 to exit");
+
+
+        String actionCode = scanner.next();
+
+        switch (actionCode) {
+            case "1":
+                System.out.println("You have chosen to input");
+                System.out.println("Which input method should be selected?");
+                System.out.println("\t\t1 from File : " + "*File directory*");
+                System.out.println("\t\t2 random");
+                System.out.println("\t\t3 manually");
+                System.out.println("\t\t4 back");
+
+                String inputCode = scanner.next();
+                input(inputCode);
+                processRequest(scanner);
+                return;
+            case "2":
+                System.out.println("You have chosen to write data in file : *File directory*");
+                write();
+                processRequest(scanner);
+                return;
+            case "3":
+                System.out.println("You have chosen to sort");
+                System.out.println("Which field should be selected for sorting?");
+                System.out.println("\t\t1 name");
+                System.out.println("\t\t2 averageGrade");
+                System.out.println("\t\t3 recordBookNumber");
+                System.out.println("\t\t4 only even recordBookNumber");
+                System.out.println("\t\t5 back");
+
+                String sortCode = scanner.next();
+                sort(sortCode);
+                processRequest(scanner);
+                return;
+            case "4":
+                System.out.println("You have chosen to count entries. Please choose entry.");
+                System.out.println("\t\tname : ");
+                String name = scanner.next();
+                System.out.println("\t\taverageGrade : ");
+                String averageGrade = scanner.next();
+                System.out.println("\t\trecordBookNumber : ");
+                String recordBookNumber = scanner.next();
+
+                count(name, averageGrade, recordBookNumber, THREAD_NUMBER);
+                processRequest(scanner);
+                return;
+            case "0":
+                System.out.println("exit");
+                flag = false;
+                return;
+        }
+
+        throw new IllegalStateException("Wrong action code. Choose 1-3 or 0.");
+    }
+
+    /**
+     * Обработчик для ввода данных в коллекцию студентов.
+     *
+     * @param inputCode код стратегии ввода.
+     */
+    static void input(final String inputCode) {
+        // реализовать паттерн "Стратегия" по введённому коду стратегии, добавить валидацию этого кода.
+        System.out.println("массив введён : " + inputCode);
+    }
+
+    /**
+     * Обработчик для записи данных из коллекции в файл.
+     */
+    static void write() {
+        // реализовать паттерн "Стратегия" по введённому коду стратегии, добавить валидацию этого кода.
+        System.out.println("массив записан");
+    }
+
+    /**
+     * Многопоточный счётчик элементов
+     *
+     * @param name             {@link Student#name} для поиска
+     * @param averageGrade     {@link  Student#averageGrade} для поиска
+     * @param recordBookNumber {@link Student#recordBookNumber} для поиска
+     * @param threadNumber     выделенное количество потоков
+     */
+    static void count(final String name, final String averageGrade, final String recordBookNumber, final int threadNumber) {
+        // реализовать счётчик
+        System.out.println("Посчитан элемент : " + name + ", " + averageGrade + ", " + recordBookNumber);
+    }
+
+    /**
+     * Обработчик для сортировки коллекции студентов.
+     *
+     * @param sortCode
+     */
+    static void sort(final String sortCode) {
+        // реализовать 4 режима режима. По каждому из полей + сортировка только чётных зачётных книжек.
+        System.out.println("массив отсортирован : " + sortCode);
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Student {
+    final String name;
+    final double averageGrade;
+    final int recordBookNumber;
+
+    public Student(String name, double averageGrade, int recordBookNumber) {
+        this.name = name;
+        this.averageGrade = averageGrade;
+        this.recordBookNumber = recordBookNumber;
+    }
+
+    /**
+     * Билдер для класса {@link Student}.
+     */
+    public static class Builder {
+        private String name = "";
+        private double averageGrade = 0.0;
+        private int recordBookNumber = 0;
+
+
+        public Builder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder averageGrade(final double averageGrade) {
+            this.averageGrade = averageGrade;
+            return this;
+        }
+
+        public Builder recordBookNumber(final int recordBookNumber) {
+            this.recordBookNumber = recordBookNumber;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(name, averageGrade, recordBookNumber);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Student student = (Student) obj;
+        return name.equals(student.name) &&  averageGrade == student.averageGrade && recordBookNumber == student.recordBookNumber;
+    }
+}
