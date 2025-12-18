cd60f9e8c4f42b32f9814de4028b34966a190e08
Bogdan <rabotakuvaldoi@gmail.com>
2025-12-18 23:31:10 +0400

    Пока что просто интерфейс инпуту добавил

--- src/main/java/org/example/InputStrategy.java
+++ src/main/java/org/example/InputStrategy.java
@@ -3,5 +3,7 @@ package org.example;
 import java.util.List;
 
 public interface InputStrategy {
-        List<Student> loadData();
+        List<Student> loadData(); //сюда нужно добавить исключение?
+
+    //про стримы надо понять сюда вообще их надо или нет
 }
